7a0b2b5188d8334a19da663a58a7289717a6698f
seanauff <sean.auffinger@gmail.com>
2019-08-14 19:36:12 -0400

    change config file location

--- runklipper.py
+++ runklipper.py
@@ -7,7 +7,7 @@ import os
 
 def main():
     while 1:
-        klipper = subprocess.Popen(['/home/dwc2-klipper/klippy-env/bin/python', '/home/dwc2-klipper/klipper/klippy/klippy.py', '/home/dwc2-klipper/printer.cfg'])
+        klipper = subprocess.Popen(['/home/dwc2-klipper/klippy-env/bin/python', '/home/dwc2-klipper/klipper/klippy/klippy.py', '/home/dwc2-klipper/config/printer.cfg'])
         if klipper.wait() == 0:
             # Exited cleanly, don't sleep for long
             time.sleep(1)
